e4092b9e939c35bb4df87e38d61380ab7bfab075
rob587 <robertocamm02@yahoo.com>
2026-06-08 23:58:32 +0200

    Fix typo in fetchGitHubData parameter and enhance data extraction for languages and top repositories

--- backend/services/githubService.js
+++ backend/services/githubService.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const fetchGitHubData = async (ussername) => {
+export const fetchGitHubData = async (username) => {
   const headers = {
     Authorization: `token ${process.env.GITHUB_TOKEN}`,
     Accept: "application/vnd.github.v3+json",
@@ -15,4 +15,34 @@ export const fetchGitHubData = async (ussername) => {
     `https://api.github.com/users/${username}/repos?per_page=100&sort=updated`,
     { headers },
   );
+
+  //   estrazione dei linguaggi
+  const languageCount = {};
+  repos.forEach((repo) => {
+    if (repo.language) {
+      languageCount[repo.language] = (languageCount[repo.language] || 0) + 1;
+    }
+  });
+
+  //   ordina per frequenza
+  const languages = Object.entries(languageCount)
+    .sort((a, b) => b[1] - a[1])
+    .map(([lang]) => lang);
+
+  // Top 3 repos per stelle
+  const topRepos = repos
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .slice(0, 3)
+    .map((repo) => ({
+      name: repo.name,
+      description: repo.description,
+      stars: repo.stargazers_count,
+      url: repo.html_url,
+    }));
+
+  return {
+    totalRepos: repos.length,
+    languages,
+    topRepos,
+  };
 };
