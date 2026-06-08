117868c28a2d2cc06e804bbcee18fd6f97ed53ff
rob587 <robertocamm02@yahoo.com>
2026-06-08 23:23:42 +0200

    Add analyze route to handle candidate analysis requests

---
+++ backend/routes/analyze.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { analyzeCandidate } from "../controllers/analyzeController.js";
+
+const router = express.Router();
+
+router.post("analyze", analyzeCandidate);
+
+export default router;
