fe38eca17ebb5784f15103b10a49ede9acad2a88
rob587 <robertocamm02@yahoo.com>
2026-06-10 12:47:02 +0200

    Implement routing and add Home and Results components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Results from "./pages/Results";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results" element={<Results />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
